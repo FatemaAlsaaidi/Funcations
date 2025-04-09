4e2a4d45b51c3899d7a353168238a7a6a8560dbd
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-04-09 14:49:39 +0400

    create "Largest of Three Numbers" function

--- Program.cs
+++ Program.cs
@@ -4,14 +4,10 @@ namespace Funcations
 {
     internal class Program
     {
+        // Even or Odd function
         public static string EvenOdd(int num)
         {
-            //int num;
             int Res;
-
-            //Console.WriteLine("Enter the number:");
-            //num = int.Parse(Console.ReadLine());
-
             Res = num % 2;
 
             if (Res == 0)
@@ -23,9 +19,30 @@ namespace Funcations
                 return "The number is odd";
             }
         }
+
+        // Largest of Three Numbers function
+        public static double LargestThreeNumbers(double num1, double num2, double num3)
+        {
+            double result =0;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            }
+
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            }
+
+            else 
+            {
+                result = num3;
+            }
+
+            return result;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             Console.WriteLine("Enter the number of operation:"); 
             Console.WriteLine("1. Even or Odd ");
             Console.WriteLine("2. Largest of Three Numbers  ");
@@ -40,6 +57,20 @@ namespace Funcations
                     string res = EvenOdd(num);
                     Console.WriteLine(res);
                     break;
+
+                case 2:
+                    Console.WriteLine("Enter first number:");
+                    double num1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter second number:");
+                    double num2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter third number:");
+                    double num3 = double.Parse(Console.ReadLine());
+
+                    double result = LargestThreeNumbers(num1, num2, num3);
+                    Console.WriteLine($"The largest number is {result}");
+                    break;
+
+
             }
 
         }
